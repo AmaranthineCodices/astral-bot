fb2dd664e56c51ef66beefef415a5926da2bf2a3
Lily Brown <git@lily.fyi>
2022-08-09 00:31:44 -0700

    basic scaffolding

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,31 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::{Report, Result, WrapErr};
+
+async fn launch(mut shutdown_rx: tokio::sync::mpsc::Receiver<()>) {
+    loop {
+        tokio::select! {
+            _ = shutdown_rx.recv() => {
+                break
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install().wrap_err("Unable to install eyre hooks")?;
+    dotenv::dotenv().ok();
+
+    let (shutdown_tx, shutdown_rx) = tokio::sync::mpsc::channel(1);
+
+    tokio::spawn(launch(shutdown_rx));
+
+    match tokio::signal::ctrl_c().await {
+        Ok(()) => shutdown_tx.send(()).await?,
+        Err(err) => return Err(Report::new(err).wrap_err("Unable to listen to Ctrl-C handler")),
+    }
+
+    Ok(())
 }
